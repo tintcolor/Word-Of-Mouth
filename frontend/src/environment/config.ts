3d951c3af43fed371cf3e68e98c5cf3ef0522e9e
ant <antdjones@gmail.com>
2017-07-12 08:34:13 -0400

    redesigned css

--- frontend/src/environment/config.ts
+++ frontend/src/environment/config.ts
@@ -1,5 +1,5 @@
-export const SERVER_URL = "http://192.168.1.158:8080/";
-// export const SERVER_URL = "http://localhost:8080/";
+// export const SERVER_URL = "http://192.168.1.158:8080/";
+export const SERVER_URL = "http://localhost:8080/";
 
 
 
